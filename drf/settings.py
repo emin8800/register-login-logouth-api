e791bdc76b413577244bd02b7e5f886fa938fdcd
Emin <99510258+emin8800@users.noreply.github.com>
2024-08-04 15:35:38 +0400

    Update settings.py

--- drf/settings.py
+++ drf/settings.py
@@ -141,5 +141,5 @@ REST_FRAMEWORK = {
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf.settings')
 application = get_wsgi_application()
