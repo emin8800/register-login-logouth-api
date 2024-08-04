7b379c35bb060fcb644cc2f0e7973af4880cd6af
Emin <99510258+emin8800@users.noreply.github.com>
2024-08-04 16:01:27 +0400

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,9 @@
-# accounts/urls.py
-
 from django.urls import path
-from .views import register_user, user_login, user_logout,csrf-token
+from .views import register_user, user_login, user_logout, csrf_token  # Not csrf-token
 
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
-    path('api/csrf-token/', csrf_token, name='csrf_token'),
+    path('csrf-token/', csrf_token, name='csrf_token'),  # Correct view name
 ]
